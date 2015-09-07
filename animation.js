23c4c2cbd3ed1849fe9b3d4b47f709e7ce9ee537
Henrik Paul <henrik@vaadin.com>
2015-09-07 17:07:25 +0300

    added test case for two followers at once

--- animation.js
+++ animation.js
@@ -401,3 +401,24 @@ window.testOne = function() {
 
     window.announceNewFollowers([testingFollower]);
 };
+
+window.testTwo = function() {
+    var followers = [
+        new Follower({
+            user: {
+                display_name: "Wolfie 1",
+                logo: "http://static-cdn.jtvnw.net/jtv_user_pictures/gowolfie-profile_image-552fee08030a7106-300x300.jpeg",
+            },
+            notifications: true,
+        }),
+        new Follower({
+            user: {
+                display_name: "Wolfie 2",
+                logo: "http://static-cdn.jtvnw.net/jtv_user_pictures/gowolfie-profile_image-552fee08030a7106-300x300.jpeg",
+            },
+            notifications: false,
+        }),
+    ];
+
+    window.announceNewFollowers(followers);
+};
